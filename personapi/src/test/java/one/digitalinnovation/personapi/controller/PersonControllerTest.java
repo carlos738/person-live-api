7aaac2f3b7ce3bc1cfce733ac4b017b2c7ed8274
carlos738 <carlosasl135@hotmail.com>
2022-02-05 18:45:01 -0300

    person-live-api

---
+++ personapi/src/test/java/one/digitalinnovation/personapi/controller/PersonControllerTest.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonControllerTest {
+
+    @Test
+    void createPerson() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
